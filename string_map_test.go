9ca8f56eff9f745de155b37e857034d42be7f18b
Alessandro Diaferia <alediaferia@gmail.com>
2016-04-22 16:13:06 +0100

    Test cleanup

--- string_map_test.go
+++ string_map_test.go
@@ -6,7 +6,35 @@ import (
 
 func TestInsert(t *testing.T) {
 	m := NewMap()
-	m.Insert("Alessandro", "Diaferia", "alediaferia")
+	expectedValues := []string{ "Diaferia", "alediaferia", "adiaferia" }
+	m.Insert("Alessandro", expectedValues...)
 
-	t.Log(m.nodeForKey("Alessandro", false).data)
+	node := m.nodeForKey("Alessandro", false)
+	if testEq(expectedValues, node.data) != true {
+		t.Errorf("Unexpected value for node 'Alessandro': expected (%v), got (%v)", expectedValues, node.data)
+	}
+}
+
+/* utils */
+func testEq(a, b []string) bool {
+
+    if a == nil && b == nil { 
+        return true; 
+    }
+
+    if a == nil || b == nil { 
+        return false; 
+    }
+
+    if len(a) != len(b) {
+        return false
+    }
+
+    for i := range a {
+        if a[i] != b[i] {
+            return false
+        }
+    }
+
+    return true
 }
